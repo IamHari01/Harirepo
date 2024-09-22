ad79c2d7c17e195dd8f1ff94f5dd5aaba4b5b579
IamHari01 <gowribalan95@gmail.com>
2024-09-22 18:09:47 +0530

    feat:i added new JAVA tutorials

---
+++ ques3.java
@@ -0,0 +1,203 @@
+/*import java.util.Scanner;
+public class add {
+    public static void main(String[] args){
+        Scanner add = new Scanner(System.in);
+        int a = add.nextInt();
+        int b = add.nextInt();
+        System.out.print(a+b);
+
+        
+    }
+    
+}*/
+/*question 2
+import java.util.Scanner;
+public class ques2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name  = scan.nextLine();
+        int age = scan.nextInt();
+        scan.nextLine();
+        String address = scan.nextLine();
+        System.out.println("my name is" + name + "my age is"+ age + "my add is " + address);
+
+        
+    }
+}*/
+/*ques3
+import java.util.Scanner;
+public class ques3{
+    public static void main(String[] args) {
+        //scenerio:1 its going to rain
+        //scenerio :2 no rain
+        boolean rain = true;
+
+        if (true){
+
+            System.out.println("take umbrella");
+        }
+        else{
+            System.out.println("no need to take umbrella");
+
+        }
+       
+
+        
+    }
+}*/
+/*boolean expression
+import java.util.Scanner;
+public class ques3{
+    public static void main(String[] args) {
+    System.out.println(3<5);
+
+    }
+}*/
+/*comparision
+import java.util.Scanner;
+public class ques3{
+    public static void main(String[] args) {
+        int num1  = 40;
+        int num2 = 50;
+        if (num1>num2){
+            System.out.println("num 1 is greater");
+
+        }
+        else{
+            System.out.println("num2 is greater");
+        }
+
+
+    }
+}*/
+/*input from user
+import java.util.Scanner;
+public class ques3{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        if (num1>num2){
+            System.out.println("num 1 is greater");
+
+        }
+        else{
+            System.out.println("num2 is greater");
+        }
+
+
+    }
+        
+    }*/
+    /*string comparision
+    import java.util.Scanner;
+public class ques3{
+    public static void main(String[] args) {
+        String a1 = new String("selva");
+
+        String a2 =new String ("selva");
+        System.out.println(a1.equals(a2));
+        System.out.println(a1==a2);
+
+
+
+    
+    }
+  }*/
+  /*Meghana problem
+  import java.util.Scanner;
+public class ques3{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String Meghana = scan.nextLine();
+        if (Meghana.equals("dead")){
+            System.out.println("surya workout started");
+
+        }
+        else{
+            System.out.println("surya has a round belly fat");
+        }
+
+        }
+
+
+    }*/
+    /*Logical operators
+    import java.util.Scanner;
+    public class ques3{
+        public static void main(String[] args) { 
+            boolean rain = false;
+            boolean cloud  = false;
+            if (rain || cloud){
+                System.out.println("jolly");
+
+            }
+            else{
+                System.out.println("bad mood");
+            }
+            
+           
+
+        }
+    }*/
+    /*Num Divisible by 3 and 5
+    import java.util.Scanner;
+    public class ques3{
+        public static void main(String[] args) {  
+            c
+            int num = scan.nextInt();
+            if(num%3==0 && num%5==0){
+                System.out.println("it is divisible by 3 and 5");
+
+            }else{
+                System.out.println("it is not that number");
+            }
+
+
+            
+
+        }
+    }*/
+    /*STUDENT MARK CHECKER
+    import java.util.Scanner;
+    public class ques3{
+        public static void main(String[] args) { 
+            Scanner scan = new Scanner(System.in);
+            int s1 = scan.nextInt();
+            int s2 = scan.nextInt();
+            int s3 = scan.nextInt();
+            int s4 = scan.nextInt();
+            int s5 = scan.nextInt();
+            int totalmarks = s1+s2+s3+s4+s5;
+            int avg = totalmarks/5;
+            if(avg<35){
+                System.out.println("ADDITIONAL SUBJECT REQUIRED");
+
+            }
+            else{
+                System.out.println("you are good to go");
+
+
+        }
+        }
+    }*/
+    /*TERNERY OPERATION
+    import java.util.Scanner;
+    public class ques3{
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+            String result = a<b?"b is greater":"a is greater";
+                System.out.println( result);
+
+        }
+    }*/
+    //FOR LOOP
+    
+    import java.util.Scanner;
+    public class ques3{
+        public static void main(String[] args) {
+            
+        }
+    }
