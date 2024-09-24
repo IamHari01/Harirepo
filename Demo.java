b0b7cd24137504038dbf608a4296e4c0c637799c
IamHari01 <gowribalan95@gmail.com>
2024-09-24 10:38:56 +0530

    feat: I added the java file

---
+++ Demo.java
@@ -0,0 +1,89 @@
+//CLASS &OBJECT CREATION
+/*public class Class {
+    String name = "";
+    int age = 0;
+    int reg_no = 0;
+     public static void main(String[] args) {
+        Class stud1 = new Class();
+        stud1.name = "selva";
+        stud1.age = 20;
+        stud1.reg_no = 43;
+         Class stud2 = new Class();
+         stud2.name = "arvind";
+         stud2.age = 18;
+         stud2.reg_no = 45;
+        
+
+            System.out.println(stud2.age);
+            
+        }
+    }*/
+    
+//FUNCTION CREATION
+    /*import java.util.Scanner; 
+    public class Class{
+        void iphone(){
+            System.out.println("I am iphone");
+        }
+        void samsung(){
+            System.out.println("Iam samsung");
+        }
+        void oppo(){
+            System.out.println("Iam oppo");
+        }
+        public static void main(String[] args) {
+            Class iphone = new Class();
+            Class samsung = new Class();
+            Class oppo = new Class();
+            samsung.samsung();
+            iphone.iphone();
+            oppo.oppo();
+
+
+            
+        }
+    }*/
+     /*FUNCTIONM WITH PARAMETER
+     import java.util.Scanner; 
+    public class Class{
+        void sum(int a ,int b){
+            System.out.println(a+b);
+
+        }
+        void multiply(int a,int b){
+            System.out.println(a*b);
+        }
+
+           public static void main(String[] args){
+             Class sum = new Class();
+             Class multiply =  new Class();
+             sum.sum(2,6);
+             multiply.multiply(5,8);
+
+
+           }
+    }           */
+    
+
+public class Demo {
+	int marks;
+	String name;
+	 Demo(int marks){
+		System.out.println("hello");
+		
+	
+		
+	}
+	
+	public static void main(String[] args) {
+	Demo obj1 = new Demo(32);
+	System.out.println(obj1.marks);
+	
+		
+		
+		
+		
+	}
+
+}
+
