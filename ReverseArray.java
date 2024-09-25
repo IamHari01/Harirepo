56246db35185c2393b3e1b025cf01ae7a71fde1e
IamHari01 <gowribalan95@gmail.com>
2024-09-25 18:36:59 +0530

    feat: i added new java file(data structure)

---
+++ ReverseArray.java
@@ -0,0 +1,49 @@
+
+
+import java.util.Scanner;
+
+public class ReverseArray {
+	public ReverseArray(int[] numbers,int start,int end){
+		while(start<end) {
+			
+			int temp = numbers[start];
+			numbers[start] = numbers[end];
+			numbers[end] = temp;
+			start++;
+			end--;
+			
+		}
+		
+	}
+	
+	
+
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the length of the array: ");
+		int n = scan.nextInt();
+		
+		System.out.println("Enter the elements of the array;");
+		int[] array = new int[n];
+		
+		for(int i = 0;i<n;i++) {
+			array[i] = scan.nextInt();
+			ReverseArray reverse = new ReverseArray(array,0,n-1);
+			
+			
+			
+		}
+		System.out.println("Reversed Array");
+		for(int output:array) {
+			System.out.println(output+" ");
+			
+		}
+	   
+		// TODO Auto-generated method stub
+
+	}
+
+}
